939986f5455855339ae3948d19026e48a599b08a
zhaidw <zhaidw@jiucai.org>
2014-01-10 10:20:34 +0800

    add holder

---
+++ src/main/java/org/jiucai/appframework/base/util/AppContextHolder.java
@@ -0,0 +1,59 @@
+package org.jiucai.appframework.base.util;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AppContextHolder {
+
+	private static final ThreadLocal<HttpServletRequest> requestHolder = new ThreadLocal<HttpServletRequest>();
+	private static final ThreadLocal<HttpServletResponse> responseHolder = new ThreadLocal<HttpServletResponse>();
+
+	public static HttpServletRequest getRequest() {
+		return requestHolder.get();
+	}
+
+	public static HttpServletResponse getResponse() {
+		return responseHolder.get();
+	}
+
+	public static HttpSession getSession() {
+		HttpSession session = null;
+		HttpServletRequest request = requestHolder.get();
+
+		if (null != request) {
+			session = request.getSession(true);
+		}
+
+		return session;
+	}
+
+	public static void setRequest(HttpServletRequest request) {
+		requestHolder.set(request);
+	}
+
+	public static void setResponse(HttpServletResponse response) {
+		responseHolder.set(response);
+	}
+
+	public static void removeRequest() {
+
+		requestHolder.remove();
+
+	}
+
+	public static void removeResponse() {
+
+		responseHolder.remove();
+
+	}
+
+	public static ThreadLocal<HttpServletRequest> getRequestHolder() {
+		return requestHolder;
+	}
+
+	public static ThreadLocal<HttpServletResponse> getResponseHolder() {
+		return responseHolder;
+	}
+
+}
